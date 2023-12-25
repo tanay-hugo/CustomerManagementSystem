898f9dd9153e43aa8a5aec56921a57d9db9cd127
Tanayyyyy <manntanayverma@gmail.com>
2023-12-25 21:37:51 +0530

    Spring Boot Basic CRUD application

---
+++ src/main/java/com/tanay/cms/exception/ApplicationError.java
@@ -0,0 +1,22 @@
+package com.tanay.cms.exception;
+
+public class ApplicationError {
+    public int code;
+    public String Message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+}
